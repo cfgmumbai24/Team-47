22be736882b185bc3c1b8d6da6649256aa047be3
Anushka Srivastava <anushka22086@iiitd.ac.in>
2024-06-15 19:18:45 +0530

    dummy data

---
+++ GoatMitra/server/data/GoatMitra.js
@@ -0,0 +1,9 @@
+let GoatMitra = [
+    {
+        username : "Mitra1",
+        name : "Ram Verma",
+        area : "Goregaon",
+        password : "mitra1",
+        phoneNumber : "9876543210"
+    }
+]
