b600ca01c17de3994c23be80ac14534b04dcb863
AnanyaJindal1145 <ajindal1145@gmail.com>
2024-06-16 08:05:46 +0530

    minor changes

--- GoatMitra/server/db/index.js
+++ GoatMitra/server/db/index.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    const connection = await mongoose.connect(`mongodb+srv://jashanmaybe75:zJNuagx59eZ4Imk1@cluster0.uy4nu8u.mongodb.net/goats`);
+    const mongoURI=process.env.MONGODB_URL;
+    const connection = await mongoose.connect(mongoURI);
     console.log(`MongoDB connected : ${connection.connection.host}`);
-    // await Team.updateMany({}, { $set: { solvedQuestions: [] } });
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
     process.exit(1);
